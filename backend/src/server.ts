b1aa7f9151ae713e8a8d3f62c4c29ff42508768f
Leonardo Barbi <ldevltda@gmail.com>
2025-09-03 10:33:29 -0300

    ajuste server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,9 +8,10 @@ import { startQueueProcess } from "./queues";
 import { TransferTicketQueue } from "./wbotTransferTicketQueue";
 import cron from "node-cron";
 
-const server = app.listen(process.env.PORT, async () => {
+const server = app.listen(process.env.PORT, '0.0.0.0', async () => {
   const companies = await Company.findAll();
   const allPromises: any[] = [];
+  
   companies.map(async c => {
     const promise = StartAllWhatsAppsSessions(c.id);
     allPromises.push(promise);
@@ -19,21 +20,17 @@ const server = app.listen(process.env.PORT, async () => {
   Promise.all(allPromises).then(() => {
     startQueueProcess();
   });
+
   logger.info(`Server started on port: ${process.env.PORT}`);
 });
 
 cron.schedule("* * * * *", async () => {
-
   try {
-    // console.log("Running a job at 01:00 at America/Sao_Paulo timezone")
-    logger.info(`Serviço de transferencia de tickets iniciado`);
-
+    logger.info(`Serviço de transferência de tickets iniciado`);
     await TransferTicketQueue();
-  }
-  catch (error) {
+  } catch (error) {
     logger.error(error);
   }
-
 });
 
 initIO(server);
